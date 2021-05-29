12d4890538f0b965ef365ef846f7cf29396ff40a
Gosha <begoshaaaa@gmail.com>
2021-05-29 11:36:04 +0600

    image and album models are added

--- source/gallery/settings.py
+++ source/gallery/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # custom apps
+    'webapp',
+    'accounts',
+    # lib apps
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_URL = 'uploads/'
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,64 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        verbose_name='Author',
+        related_name='image',
+        null=False,
+        blank=False
+    )
+
+    image = models.ImageField(
+        upload_to='images',
+        null=False,
+        blank=False,
+        verbose_name='Image'
+    )
+    description = models.CharField(
+        null=False,
+        blank=False,
+        max_length=200,
+        verbose_name='Description'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        db_table = 'images'
+        verbose_name = 'Image'
+        verbose_name_plural = 'Images'
+
+class Album(models.Model):
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        verbose_name='Author',
+        related_name='album',
+        null=False,
+        blank=False
+    )
+    name = models.CharField(
+        null=False,
+        blank=False,
+        max_length=60,
+        verbose_name='Album Name'
+    )
+    description = models.TextField(
+        blank=True,
+        verbose_name='Album Description'
+    )
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        db_table = 'albums'
+        verbose_name = 'Album'
+        verbose_name_plural = 'Albums'
+
